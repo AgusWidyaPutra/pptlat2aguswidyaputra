e2a16fc66a8bda77f6314724cf8603dd07ad78ba
AgusWidyaPutra <awp170802@gmail.com>
2021-09-29 22:20:53 +0700

    pptLat2AgusWidyaPutra
    PPT LATIHAN 2 / SLIDE 19

---
+++ lat2.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD>
+<TITLE> Contoh Skirp PHP </TITLE>
+<BODY>
+<? 
+        $a = 3;
+        $b = 7;
+        $a += 5;
+        $b = ($c = 11) + 3;
+        echo "Nilai variabel a adahal = $a";
+        echo "<BR>";
+        echo "Nilai variabel b adalah = $b";
+        echo "<BR>";
+        echo "Nilai variabel c adalah = $c";
+?>
+</BODY>
+</HTML>
